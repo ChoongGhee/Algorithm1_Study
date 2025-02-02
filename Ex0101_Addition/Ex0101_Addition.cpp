f4684611c2ae9eab279c6a9e03aea9c6d27ea96f
ChoongGhee <tititito44@gmail.com>
2025-02-03 02:10:49 +0900

    ex101 comlete 25.01.27

--- Ex0101_Addition/Ex0101_Addition.cpp
+++ Ex0101_Addition/Ex0101_Addition.cpp
@@ -4,11 +4,55 @@
 
 using namespace std;
 
-string Add(string str1, string str2)
-{
-	// TODO:
+// 문제 : 오버플로우 예외를 계산하지 못해서 문자를 하나하나 추가하는 형식 그 자릿수의, 그래서 로직이 많이 바뀌어야함.
+// string Add(string str1, string str2)
+// {
+// 	// TODO:
+// 	int exp = 1;
+// 	int str1_idx = str1.size()-1;
+// 	int str2_idx = str2.size()-1;
+// 	int result = 0;
 
-	return string("0");
+// 	while(str1_idx > -1 || str2_idx > -1){
+//         if(str1_idx > -1 && str2_idx > -1){           // 둘 다 유효할 때
+//             result += ((str1[str1_idx] - '0') + (str2[str2_idx] - '0')) * exp;
+//             str1_idx--;
+//             str2_idx--;
+//         }
+//         else if(str1_idx > -1){                       // str1만 남았을 때
+//             result += (str1[str1_idx] - '0') * exp;
+//             str1_idx--;
+//         }
+//         else if(str2_idx > -1){                       // str2만 남았을 때
+//             result += (str2[str2_idx] - '0') * exp;
+//             str2_idx--;
+//         }
+//         exp *= 10;
+//     }
+
+// 	cout << result << endl;
+// 	return to_string(result);
+// }
+string Add(string str1, string str2) {
+    string result;
+    int carry = 0;
+    int str1_idx = str1.size() - 1;
+    int str2_idx = str2.size() - 1;
+    
+    // 한 자리씩 더하기
+    while (str1_idx >= 0 || str2_idx >= 0 || carry) {
+        int sum = carry;
+        
+        if (str1_idx >= 0)
+            sum += (str1[str1_idx--] - '0');
+        if (str2_idx >= 0)
+            sum += (str2[str2_idx--] - '0');
+        
+        carry = sum / 10;        // 다음 자리로 올림
+        result = char(sum % 10 + '0') + result;  // 현재 자리 숫자
+    }
+    
+    return result;
 }
 
 int main()
