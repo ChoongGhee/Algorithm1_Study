28ffd8ec4222f1cd0d6b975c4bddb140d5e63931
ChoongGhee <tititito44@gmail.com>
2025-02-03 02:11:13 +0900

    ex102 complete 25.01.28

--- Ex0102_Subtraction/Ex0102_Subtraction.cpp
+++ Ex0102_Subtraction/Ex0102_Subtraction.cpp
@@ -17,6 +17,45 @@ string Subtract(string str1, string str2)
 
 	string result(N, '0');
 
+	int carry = 0;
+
+    for(int i = N-1; i >= 0; i--) {
+		// 1/28 오후 4시 30분 해당 부분은 carry부분은 맞는데 carry를 더하고 비교를 해야하기 때문에 잘못되었음. 비교 후 값을 반영하니까.
+        // if(str1[i] < str2[i]){
+        //     result[i] = (str1[i] - '0') - (str2[i] - '0') + 10 + carry + '0';
+        //     carry = -1;
+        // }
+        // else {
+        //     result[i] = (str1[i] - '0') - (str2[i] - '0') + carry + '0';
+        //     carry = 0;
+        // }
+		int sum = carry + str1[i] - str2[i];
+		if(sum < 0){
+			sum += 10;
+			carry = -1;
+		}
+		// 1/28 밤12시 : 해당 부분을 빼먹어서 업데이트가 이전 값을 반영해서 작동해서 문제였음.
+		else{
+			carry = 0;
+		}
+		result[i] = char(sum + '0');
+
+		// 정답코드 (1/29 보기)
+		int sum = carry + str1[i] - str2[i] + 10;
+		carry = sum / 10 - 1; // 미리 빌렸던 10을 -1로 반영
+        result[i] = char(sum % 10 + '0');
+
+    }
+
+	for(int i = 0;i<N;i++){
+		if(result[i] != '0'){
+			result = result.substr(i);
+			break;
+		}
+	}
+
+	if(result.empty()) return "0";
+
 	// TODO: 더하기와 거의 비슷합니다.
 
 	// 불필요한 '0' 제거 (예: "078" -> "78")
