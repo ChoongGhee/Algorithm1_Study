f47118402568a5c20633da7f7a80ef65edf3f2a0
ChoongGhee <tititito44@gmail.com>
2025-02-03 02:10:01 +0900

    ex2 complete 25.01.26

--- Ex0002_WarmingUp/Ex0002_WarmingUp.cpp
+++ Ex0002_WarmingUp/Ex0002_WarmingUp.cpp
@@ -10,6 +10,15 @@ int main()
 		string binary;
 
 		//TODO:
+		while (decimal > 0) {
+			// 2로 나눈 나머지가 0인지 1인지 확인
+			if (decimal % 2 == 0)
+				binary = '0' + binary;  // 앞에 붙여줌
+			else
+				binary = '1' + binary;  // 앞에 붙여줌
+			
+			decimal = decimal / 2;
+		}
 
 		cout << binary << endl; // 1101001
 	}
@@ -19,17 +28,38 @@ int main()
 		string input = "Hello, World!";
 
 		// TODO:
+		for(int i = 0;i<(input.size()-1)/2;i++)
+			swap(input[i], input[input.size()-1-i]);
 
 		cout << input << endl; // !dlroW ,olleH
 	}
 
 	// 문제 3. 모든 자리 다 더하기
+	// 이건 문제를 이해하지 못함. 자리수가 1이 될때 까지 더하는 것임.
 	{
 		string n = "789789";
 
 		// TODO:
+		// int sum = 0;
+		// for(char c : n){
+		// 	sum +=stoi(string(1, c));
+		// }
+		// cout << sum << endl;
+		
+		while (n.size() > 1)
+        {
+            int sum = 0;
+            while (n.size() > 0)
+            {
+                sum += (n[0] - '0') % 10;
+                n = n.substr(1, n.size() - 1);
+            }
+            cout << sum << endl;
+            n = to_string(sum);
+        }
+        cout << n << endl; // 3 <- 기대 출력
 
-		cout << n << endl;
+		
 	}
 
 	return 0;
