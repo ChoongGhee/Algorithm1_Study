3a6c95b2d9c325865f669b915fc0b6242ad4c684
ChoongGhee <tititito44@gmail.com>
2025-03-19 18:15:53 +0900

    ex602 complete

--- Ex0602_RadixSort/Ex0602_RadixSort.cpp
+++ Ex0602_RadixSort/Ex0602_RadixSort.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <numeric>
 #include <iomanip>
+
+#include <algorithm>
 using namespace std;
 
 void Print(vector<int>& arr)
@@ -22,30 +24,67 @@ void CountingSort(vector<int>& arr, int k, int exp)
 
 	vector<int> count(k + 1, 0);
 	// TODO:
+	int divisor = 1;
+    for(int i=1; i<exp; i++) divisor *= 10;
 
-	for (int i = arr.size() - 1; i >= 0; i--)
-	{
-		// TODO:
-	}
+	for(int i=0;i<temp.size();i++) count[(temp[i] / divisor) % 10]++;
+
+	for(int i=1;i<count.size();i++) count[i] += count[i-1];
+
+	for (int i = arr.size() - 1; i >= 0; i--) arr[--count[(temp[i] / divisor) % 10]] = temp[i];
 }
+
+
+// 최적화 전 코드 작동은 함.
+// void CountingSort(vector<int>& arr, int k, int exp)
+// {
+//     vector<int> temp = arr; // 복사
+
+//     vector<int> count(k + 1, 0);
+//     // TODO:
+//     for(int i=0;i<temp.size();i++) {
+//         int idx = temp[i];
+//         for(int j=0; j<exp-1;j++) idx/=10;
+//         count[idx%10]++;
+//     }
+
+//     for(int i=1;i<count.size();i++) count[i] += count[i-1];
+
+//     for (int i = arr.size() - 1; i >= 0; i--)
+//     {
+//         // TODO:
+//         int idx = temp[i];
+//         for(int j=0; j<exp-1;j++) idx/=10;
+
+//         arr[--count[idx%10]] = temp[i];
+//     }
+// }
 
 void RadixSort(vector<int>& arr)
 {
 	int k = 9; // 0 이상 9 이하
 	int m = *max_element(arr.begin(), arr.end());
+	// TODO:
+	int exp = 1;
 
-	//for (TODO)
-	//{
-	//	cout << "exp = " << exp << endl;
-	//	TODO:
-	//	Print(arr);
-	//}
+	while(m/10 > 0) {
+		m/=10;
+		exp++;
+	}
+
+	for(int i=1;i<=exp;i++)
+	{
+		cout << "exp = " << exp << endl;
+		// TODO:
+		CountingSort(arr, k, i);
+		Print(arr);
+	}
 }
 
 int main()
 {
-	vector<int> arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
-	//vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
+	// vector<int> arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+	vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
 
 	Print(arr);
 
