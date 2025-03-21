e0d5da5cb2d28d37ca8a37155be129107ea6033e
ChoongGhee <tititito44@gmail.com>
2025-03-21 23:31:49 +0900

    ex603 coplelte

--- Ex0603_SortingWords/Ex0603_SortingWords.cpp
+++ Ex0603_SortingWords/Ex0603_SortingWords.cpp
@@ -19,15 +19,30 @@ void Print(vector<T>& arr)
 
 void CountingSort(vector<string>& arr, int k, int d)
 {
-	// vector<string> temp = arr; // 복사
-	// std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
+	vector<string> temp = arr; // 복사
 
+	std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
+	
 	// TODO:
+	vector<int> count(k + 1, 0);
+
+	for(int i=0;i<temp.size();i++) count[temp[i][d] - 'A']++;
+
+	for(int i=1;i<count.size();i++) count[i] += count[i-1];
+
+	for (int i = temp.size() - 1; i >= 0; i--) arr[--count[temp[i][d]-'A']] = temp[i];
 }
 
 void RadixSort(vector<string>& arr)
 {
 	// TODO:
+	int exp = 1;
+	for(auto data : arr) if(exp < data.size()) exp = data.size();
+
+	// 90 - 65  = 25임. 실제 영어 갯수는 26개라서 + 1;
+	int arr_range = 'Z' - 'A' + 1;
+
+	for(int i = exp-1; -1<i;i--) CountingSort(arr, arr_range, i);
 }
 
 int main()
